4b4c386a4411578a1784619172c8ca32cae27b1d
Denis <dmazanov@mail.ru>
2017-07-11 18:09:12 +0200

    Initial commit

---
+++ src/searchform.php
@@ -0,0 +1,7 @@
+<!-- search -->
+<form class="search-form" method="get" action="<?php echo home_url(); ?>">
+	<div role="search">
+		<input class="search-input" type="search" name="s" aria-label="Search site for:" placeholder="<?php _e('To search, type and hit enter.', 'basictheme'); ?>">
+		<button class="search-submit" type="submit"><?php _e('Search', 'basictheme'); ?></button>
+	</div>
+</form>
