13f55c925caef0894087c876a804a7aecc2ca4e1
Denis <dmazanov@mail.ru>
2017-08-14 19:42:52 +0200

    install gulp-sass and gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,13 @@
 "use strict";
-var gulp = require("gulp");
+var gulp = require("gulp"),
+		sass = require("gulp-sass"),
+		autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("sass", function() {
+	return gulp.src("src/scss/**/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest("src/css"));
+});
+
+gulp.task("default", ["sass"]);
