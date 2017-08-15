6fb50dc75e7eee828b0e5a4cae1e50c7747bccf3
Denis <dmazanov@mail.ru>
2017-08-15 13:06:53 +0200

    watching files

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,24 @@
 var gulp = require("gulp"),
 		sass = require("gulp-sass"),
 		autoprefixer = require("gulp-autoprefixer"),
-		rename = require("gulp-rename");
+		rename = require("gulp-rename"),
+		browserSync = require("browser-sync").create(),
+		reload = browserSync.reload;
 
 gulp.task("sass", function() {
 	return gulp.src("src/scss/**/*.scss")
 		.pipe(sass().on("error", sass.logError))
 		.pipe(autoprefixer())
 		.pipe(gulp.dest("src/css"));
+});
+
+// Static Server + watching scss/php files
+gulp.task('browser-sync', function() {
+		browserSync.init({
+			files: ['./**/*.php'],
+			proxy: 'wp-basic-theme.loc/',
+		});
+	gulp.watch("src/scss/**/*.scss", ["sass"]);
 });
 
 gulp.task("default", ["sass"]);
