1455496ce2412a280607df74e43130faf529e225
Denis <dmazanov@mail.ru>
2017-08-15 14:21:35 +0200

    Images. install gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,11 @@ var gulp = require("gulp"),
 		plumber = require("gulp-plumber"),
 		gutil = require("gulp-util"),
 		browserSync = require("browser-sync").create(),
-		reload = browserSync.reload;
+		reload = browserSync.reload,
+		concat = require("gulp-concat"),
+		jshint = require("gulp-jshint"),
+		uglify = require("gulp-uglify"),
+		imagemin = require("gulp-imagemin");
 
 
 var onError = function(err) {
@@ -33,4 +37,19 @@ gulp.task('browser-sync', function() {
 	gulp.watch("src/js/*.js").on("change", reload);
 });
 
-gulp.task("default", ["sass"]);
+gulp.task("default", ["sass", "browser-sync"]);
+
+
+
+// ====================================================
+// ====================================================
+// ================= Build DIST ======================
+
+
+
+gulp.task("images", function() {
+	return gulp.src("src/img/**/*")
+	.pipe(plumber({errorHandler: onError}))
+	.pipe(imagemin({optimizationLevel: 7, progressive: true}))
+	.pipe(gulp.dest("dist/img"))
+})
